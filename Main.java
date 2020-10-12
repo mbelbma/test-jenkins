662e171fb79d700bd5d7c7128bf867d5361ff09f
mbelbma <46251150+mbelbma@users.noreply.github.com>
2020-10-12 19:57:06 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Mohamed Belasri LOLO");
+        System.out.println("Hello, Mohamed Belasri jenkisfile");
       
     }
 }
