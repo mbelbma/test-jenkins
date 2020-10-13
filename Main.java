45d00cc1ccb59e01742e2861ab7956f058619056
mbelbma <46251150+mbelbma@users.noreply.github.com>
2020-10-13 10:31:06 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Mohamed Belasri jenkisfile");
+        System.out.println("Hello, test docker");
       
     }
 }
